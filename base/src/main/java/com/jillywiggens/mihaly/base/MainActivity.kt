36c4044d6e7ddb31d56f44df050ead32c74f0be0
Bobby Gibbs <bobbyG27@cox.net>
2018-04-24 19:49:43 -0500

    Add Dagger2 and inject presenter

--- base/src/main/java/com/jillywiggens/mihaly/base/MainActivity.kt
+++ base/src/main/java/com/jillywiggens/mihaly/base/MainActivity.kt
@@ -2,13 +2,19 @@ package com.jillywiggens.mihaly.base
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.jillywiggens.mihaly.base.menu.DaggerMenuPresenterFactory
 import com.jillywiggens.mihaly.base.menu.MenuPresenter
+import javax.inject.Inject
 
 class MainActivity : AppCompatActivity() {
 
+    @Inject lateinit var presenter : MenuPresenter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val presenter = MenuPresenter()
+        val presenter = DaggerMenuPresenterFactory.builder()
+                .build()
+                .injectPresenter()
         setContentView(presenter.presentView(baseContext))
     }
 }
