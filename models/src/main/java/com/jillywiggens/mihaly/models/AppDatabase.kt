2fb390f00a10d3949d14dc8147a6d3c3ff54cd40
Bobby Gibbs <bobbyG27@cox.net>
2020-02-08 17:39:54 -0500

    Fix migration of local database to handle new finished column

--- models/src/main/java/com/jillywiggens/mihaly/models/AppDatabase.kt
+++ models/src/main/java/com/jillywiggens/mihaly/models/AppDatabase.kt
@@ -2,10 +2,21 @@ package com.jillywiggens.mihaly.models
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.jillywiggens.mihaly.models.books.Book
 import com.jillywiggens.mihaly.models.books.BookDao
+
 
 @Database(entities = [Book::class], version = 2)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun bookDao(): BookDao
+
+    companion object {
+        val Migration_1_2: Migration = object : Migration(1, 2) {
+            override fun migrate(database: SupportSQLiteDatabase) {
+                database.execSQL("ALTER TABLE book ADD COLUMN finished INTEGER NOT NULL DEFAULT '0'")
+            }
+        }
+    }
 }
--- models/src/main/java/com/jillywiggens/mihaly/models/Presenter.kt
+++ models/src/main/java/com/jillywiggens/mihaly/models/Presenter.kt
@@ -2,6 +2,7 @@ package com.jillywiggens.mihaly.models
 
 import android.content.Context
 import androidx.room.Room
+import com.jillywiggens.mihaly.models.AppDatabase.Companion.Migration_1_2
 
 /**
  * Created by bobby on 3/25/2018.
@@ -10,5 +11,7 @@ abstract class Presenter {
     abstract val TAG: String
     abstract fun createView(): ViewDelegate
     open fun destroyView() { }
-    fun generateDatabase(context: Context) = Room.databaseBuilder(context, AppDatabase::class.java, "mihaly").build()
+    fun generateDatabase(context: Context) = Room.databaseBuilder(context, AppDatabase::class.java, "mihaly")
+            .addMigrations(Migration_1_2)
+            .build()
 }
